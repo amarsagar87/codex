d0a99e4e64071ade317e51150ad47b3652bde8f3
Amar Sagar <amar@Amars-MacBook-Pro.local>
2023-02-07 16:59:55 +0400

    update the server url

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('http://localhost:4001/', {
+    const response = await fetch('https://codex-13ld.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
